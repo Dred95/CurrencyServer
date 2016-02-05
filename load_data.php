2d870bc993df3a25a3bf865f8f9b0d472bf23fe1
dred <dred_95@mail.ru>
2016-02-06 00:00:36 +0600

    Added parsing instaforex.com and pushing to DB

---
+++ load_data.php
@@ -0,0 +1,30 @@
+<?php 
+	ini_set('display_errors','On');
+	error_reporting(E_ALL &~ E_DEPRECATED);
+	header('Content-Type: text/html; charset=UTF-8');
+
+	$myCurl = curl_init();
+	curl_setopt_array($myCurl, array(
+			CURLOPT_URL => 'https://quotes.instaforex.com/get_quotes.php?q=eurusd&m=json',
+			CURLOPT_TIMEOUT_MS => 3100,
+			CURLOPT_RETURNTRANSFER => true
+			));
+	$response = curl_exec($myCurl);
+        curl_close($myCurl);
+	
+	if (strlen($response)<5)
+	{
+		exit;
+	}
+
+	$json = json_decode ($response, true);
+
+	$link = mysql_connect('localhost','adminSUjpCBH','DMWc-acIGwrP');
+	mysql_select_db('currency');
+	mysql_set_charset('utf8');
+
+	$query =" INSERT INTO eurusd (time, bid, ask) VALUES(".$json["lasttime"].",".$json["bid"].",".$json["ask"].")";
+	echo $qery;
+	mysql_query($query);
+?>
+
