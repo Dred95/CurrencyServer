1663b60c560f30a39d77976a41ea2b1425f18c51
dred <dred_95@mail.ru>
2016-03-04 19:22:16 +0600

    Add desk to query

--- get_prediction.php
+++ get_prediction.php
@@ -7,7 +7,7 @@
 	mysql_select_db('currency');
 	mysql_set_charset('utf8');
 
-	$query ="SELECT `value` FROM `eurusdprediction` ORDER BY `time`";
+	$query ="SELECT `value` FROM `eurusdprediction` ORDER BY `time` DESK";
 	$result = mysql_query($query);
 	echo mysql_result($result, 0);
 ?>
