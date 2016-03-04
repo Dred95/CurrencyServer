a7e157790f70f25486f5111ddfbc8600e822dd91
dred <dred_95@mail.ru>
2016-03-04 19:28:33 +0600

    Fix Desc

--- get_prediction.php
+++ get_prediction.php
@@ -7,7 +7,7 @@
 	mysql_select_db('currency');
 	mysql_set_charset('utf8');
 
-	$query ="SELECT `value` FROM `eurusdprediction` ORDER BY `time` DESK";
+	$query ="SELECT `value` FROM `eurusdprediction` ORDER BY `time` DESС";
 	$result = mysql_query($query);
 	echo mysql_result($result, 0);
 ?>
