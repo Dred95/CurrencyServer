fc0d7c9bd4380761c47e8929a946407ee404f261
dred <dred_95@mail.ru>
2016-03-04 18:55:27 +0600

    Echo mysql_error

--- get_prediction.php
+++ get_prediction.php
@@ -7,11 +7,11 @@
 	mysql_select_db('currency');
 	mysql_set_charset('utf8');
 
-	$query ="SELECT `value` FROM `eurusdprediction` ORDER BY `time` DESK LIMIT 1";
+	$query ="SELECT `value` FROM `eurusdprediction` ORDER BY `time` DESK LIMIT 0,1";
 	$result = mysql_query($query);
 	if ($result != 1)
 	{
-		echo $result."error".$query;
+		echo $result."error".mysql_error($link);
 	}else
 	{
 		echo $result;
