de00847714a45d794213dafb9a33b3e814827b66
dred <dred_95@mail.ru>
2016-03-04 18:33:21 +0600

    Add scripts for get/set prediction.

---
+++ get_prediction.php
@@ -0,0 +1,20 @@
+<?php 
+	//ini_set('display_errors','On');
+	//error_reporting(E_ALL &~ E_DEPRECATED);
+	header('Content-Type: text/html; charset=UTF-8');
+	
+	$link = mysql_connect('localhost','adminSUjpCBH','DMWc-acIGwrP');
+	mysql_select_db('currency');
+	mysql_set_charset('utf8');
+
+	$query ="SELECT value FROM eurusdprediction ORDER BY time DESK LIMIT 1";
+	$result = mysql_query($query);
+	if ($result != 1)
+	{
+		echo $result."error".$query;
+	}else
+	{
+		echo $result;
+	}
+?>
+
